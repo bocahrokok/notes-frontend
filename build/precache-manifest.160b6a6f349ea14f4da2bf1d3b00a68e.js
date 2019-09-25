ce32710c31b29cbb81dad502cb1face838f3acb5
bocahrokok <indieunistudy@gmail.com>
2019-09-25 20:00:18 +0700

    berhasil sampai  backend url

---
+++ build/precache-manifest.160b6a6f349ea14f4da2bf1d3b00a68e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5965d7a457bafd7eb11c8f8642fe6f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6daae4d05b06b989164c",
+    "url": "/static/css/main.572801be.chunk.css"
+  },
+  {
+    "revision": "62f9dade82f13ccc60c2",
+    "url": "/static/js/2.5763f102.chunk.js"
+  },
+  {
+    "revision": "6daae4d05b06b989164c",
+    "url": "/static/js/main.e21111cd.chunk.js"
+  },
+  {
+    "revision": "f85124929e65a9264064",
+    "url": "/static/js/runtime~main.b0c36f17.js"
+  }
+]);
